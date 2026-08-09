392c4038fe95f470d74902fb248f6380429d0566
Mark Shevchuk <95185987+Traer3@users.noreply.github.com>
2026-08-09 22:17:26 +0300

    Update realm.js

--- src/db/realm.js
+++ src/db/realm.js
@@ -4,6 +4,7 @@ import { schema } from './schemas';
 export const { RealmProvider, useRealm, useQuery, useObject } = createRealmContext({
     schema: schema,
     schemaVersion: 4,
+//ебанул 5 нахуй
     /*
     onMigration:(oldRealm, newRealm) => {
         console.log("New version realm table")
