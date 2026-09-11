be3195808bfb551969cfda1791533e6717a8b6e2
Mark Shevchuk <95185987+Traer3@users.noreply.github.com>
2026-09-11 21:40:07 +0300

    Update schemas.js

--- src/db/schemas.js
+++ src/db/schemas.js
@@ -58,7 +58,7 @@ export class ExerciseWeightHistory extends Realm.Object {
         primaryKey: 'id',
         properties: {
             id: 'int',
-            //day: 'string',
+               //day: 'string',
             timestamp: 'int',
             fullName:"string",
             weightData: 'Action'
